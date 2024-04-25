0bb6ddb83fbd26b35a415bb7a4c54cd11343ae29
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2024-04-25 18:36:22 -0500

    Se recorre el arreglo de las peliculas y se pintan por medio de card con MaterialUI

---
+++ src/interface/Movie.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+    id: number,
+    title: string,
+    date_created: string,
+    character: string,
+    image: string,
+    rating: number
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import API from '../API'
+import { Movie } from '../interface/Movie';
+import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
 
 function Home() {
+    const [movies, setMovies] = useState<Movie[]>([]);
 
     useEffect(()=>{
-        API.get("/movie").then((resp)=>{
-            console.log(resp, "resp");
+        API.get<Movie[]>("/movie").then((resp)=>{
+            setMovies(resp.data);
             
         }).catch((error) =>{
             console.log(error);
@@ -13,7 +16,30 @@ function Home() {
     }, [])
 
   return (
-    <div>Home</div>
+    <>
+    <div>
+            <Grid container spacing={2}>
+                {movies.map((movie) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} key={movie.id}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                height="200"
+                                image={movie.image}
+                                alt={movie.title}
+                            />
+                            <CardContent>
+                                <Typography variant="h6">{movie.title}</Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    </>
   )
 }
 
