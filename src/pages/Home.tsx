80bb95b9791a064a05310b7972e0e81045e2a644
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2024-04-25 18:28:49 -0500

    Se configura el archivo env con la ruta de la API. Y se hace un llamado de prueba desde el page Home para comprobar que funcione.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import API from '../API'
 
 function Home() {
+
+    useEffect(()=>{
+        API.get("/movie").then((resp)=>{
+            console.log(resp, "resp");
+            
+        }).catch((error) =>{
+            console.log(error);
+        })
+    }, [])
+
   return (
     <div>Home</div>
   )
